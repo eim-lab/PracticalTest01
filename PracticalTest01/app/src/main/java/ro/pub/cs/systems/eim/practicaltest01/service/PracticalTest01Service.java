b6e82f93a56ddafdf2b3183251b3ff78b288f745
Dragoș Niculescu <dragos.niculescu@cs.pub.ro>
2024-03-27 15:35:19 +0200

    oreo upgrade

--- PracticalTest01/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/service/PracticalTest01Service.java
+++ PracticalTest01/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/service/PracticalTest01Service.java
@@ -1,14 +1,40 @@
 package ro.pub.cs.systems.eim.practicaltest01.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 
 import ro.pub.cs.systems.eim.practicaltest01.general.Constants;
 
 public class PracticalTest01Service extends Service {
 
     ProcessingThread processingThread = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            String CHANNEL_ID = "my_channel_01";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
+
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle("")
+                    .setContentText("").build();
+
+            startForeground(1, notification);
+        }
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
